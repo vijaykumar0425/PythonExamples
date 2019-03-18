90dddff5ac78064f11b92dce1f5801baf7ac64fd
vijaykumar0425 <41435942+vijaykumar0425@users.noreply.github.com>
2019-03-19 00:13:15 +0530

    Add files via upload

---
+++ add_two_numbers.py
@@ -0,0 +1,5 @@
+a = 10
+b = 20
+print(a+b)
+print(10+20)
+print("add two numbers",a+b)
---
+++ cube_of_a_number1.py
@@ -0,0 +1,7 @@
+# Python Program to Calculate Cube of a Number
+
+number = float(input(" Please Enter any numeric Value : "))
+
+cube = number ** 3
+
+print("The Cube of a Given Number {0}  = {1}".format(number, cube))
